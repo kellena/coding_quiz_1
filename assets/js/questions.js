4d1f6b8ff4364c411c27aefe27287b249bdba6c9
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-23 19:48:54 -0500

    added base html to be manipulated by js, linked scripts

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        question: "1: insert question here?",
+        answers: ["a","b","c","d"],
+        correct: "b"
+    },{
+        question: "2: insert question here?",
+        answers: ["a","b","c","d"],
+        correct: "c"
+    },{
+        question: "3: insert question here?",
+        answers: ["a","b","c","d"],
+        correct: "a"
+    },{
+        question: "4: insert question here?",
+        answers: ["a","b","c","d"],
+        correct: "d"
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// create a start button that starts the timer and shows the first question
+// - create start button, add eventListener on it
+// - when timer hits 0, stop timer and alert user that no time remains, generate the score as it hits 0
+// - if timer is > 0, we the timer to decrement
+// - - if timer is > 0, user should be able to select an answer to the questions
+// - - if user has selected an answer, display whether the answer is correct or not
+// - - once the correct/incorrect answer has been displayed, move on to the next question
+
+// create an array of objects for questions
