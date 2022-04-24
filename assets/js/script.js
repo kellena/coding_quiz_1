cd7cc3996922f174ff31767cfce8aa5e9f7ff62a
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-23 20:32:39 -0500

    added simple text content for start screen, began writing an eventListener on the startBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,15 @@ var answers = document.querySelector("#answers");
 
 var timeIndex = 0;
 var stopQuiz;
+
+questions.textContent = "Basic Coding Quiz"
+answers.textContent = "Click the start button to begin the quiz!"
+
+// hide quiz until start has been clicked
+startQuiz.addEventListener("click", function(){
+    timerOn();
+    qVisible();
+    startQuiz.setAttribute("style", "display: none")
+})
+
+// flesh out timerOn and qVisible functions
