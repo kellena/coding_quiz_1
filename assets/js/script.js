e028fee76f2e24cc82e4e4f0e945a92cde93fc3a
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-23 21:21:47 -0500

    created a function to decrement timer and stop the quiz, as well as get the user score and initials from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 var timer = document.querySelector("#timer");
 var timeLeft = 30;
-
 var startQuiz = document.querySelector("#startBtn");
-
 var questions = document.querySelector("#questions");
 var answers = document.querySelector("#answers");
-
 var timeIndex = 0;
 var stopQuiz;
 
 questions.textContent = "Basic Coding Quiz"
 answers.textContent = "Click the start button to begin the quiz!"
 
-// hide quiz until start has been clicked
 startQuiz.addEventListener("click", function(){
     timerOn();
     qVisible();
     startQuiz.setAttribute("style", "display: none")
-})
+});
 
-// flesh out timerOn and qVisible functions
+// decrement time after quiz start, stop quiz if time = 0
+function timerOn() {
+    stopQuiz = setInterval(function(){
+        timeLeft--;
+        timer.textContent = "Time: " + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(stopQuiz);
+            questions.textContent = "High Scores:";
+            answers.innerHTML = localStorage.getItem("userInit") + " --- " + localStorage.getItem("userScore");    
+        }
+    }, 1000);
+}
