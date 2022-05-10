be32ddc1494cdf9d43bef17f835a3170e3c99eed
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-10 10:07:06 -0500

    created startQuiz function with decrememnting timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var displayQuestion = document.getElementById("quizQuestion");
 var quizDisplay = document.getElementById("quizDisplay");
 var displayAnswers = document.getElementById("quizAnswers");
 
-var timer = 60;
+var timer = 45;
 var score = 0;
 var countdown;
 var displayTimer = document.createElement("h1");
@@ -13,7 +13,35 @@ var displayScore = document.createElement("h1");
 document.body.appendChild(displayTimer);
 document.body.appendChild(displayScore);
 
-function startQuiz() {};
+
+function startQuiz() {
+
+    if (startBtn.style.display === "none") {
+        startBtn.style.display = "block";
+    } else {
+        startBtn.style.display = "none";
+    }
+
+    var countdown = setInterval(function () {
+
+        if (timer > 0) {
+            timer--;
+            displayTimer.textContent = timer;
+        } else {
+            clearInterval(countdown);
+            gameOver();
+            timer = 45;
+            displayTimer.textContent = timer;
+            localStorage.setItem("player score", score);
+            document.location.reload();
+        }
+        
+      }, 1000);
+
+      displayQuiz();
+
+};
+
 
 function displayQuiz() {};
 
