25572a3978ddaca6904e2a92d6ae7cca541acc2b
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-10 10:56:20 -0500

    built out displayQuiz functionality, wrote cursory checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ function startQuiz() {
             localStorage.setItem("player score", score);
             document.location.reload();
         }
-        
+
       }, 1000);
 
       displayQuiz();
@@ -43,9 +43,37 @@ function startQuiz() {
 };
 
 
-function displayQuiz() {};
+function displayQuiz() {
 
-function checkAnswer() {};
+    var index = Math.floor(Math.random() * questionPool.length);
+    quiz = questionPool[index];
+    questionPool.splice(index, 1);
+    displayQuestion.textContent = quiz.question;
+    removeQuiz(displayAnswers);
+    
+    for (let i = 0; i < 4; i++) {
+        quizDisplay.appendChild(displayAnswers);
+        var index = Math.floor(Math.random() * quiz.answers.length);
+        var displayAnswer = document.createElement("button");
+        displayAnswer.setAttribute("class", "answers");
+        displayAnswers.appendChild(displayAnswer);
+        displayAnswer.textContent = quiz.answers[index];
+        displayAnswer.setAttribute("id", quiz.answers[index]);
+        quiz.answers.splice(index, 1);
+    }
+
+};
+
+function checkAnswer() {
+
+    if (e.path[0].id === quiz.correct) {
+        score++;
+        displayQuiz();
+    } else {
+        timer = timer - 5;
+    }
+
+};
 
 function hideQuiz() {};
 
